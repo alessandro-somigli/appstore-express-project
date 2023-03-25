6ed682903bdaf374e86c85d8d6459f0673541282
Alessandro Somigli <alessandro.somigli.04@gmail.com>
2023-03-25 10:51:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(process.env.PORT || 3000)
